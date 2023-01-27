a31a55298fab8e6e3605b095fc17375484caeff1
Wanderberg <wdmsantana@gmail.com>
2023-01-27 12:27:29 -0400

    crud completo

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const { update } = require('../models/Person');
 const Person = require('../models/Person');
 
 
-
+// Create
 router.post('/', async (req, res) => {
     
     // req.body
@@ -73,16 +74,49 @@ router.patch('/:id', async(req, res)=>{
     const person = { 
         name,
         salary,
-        approved, 
+        approved
     }
-
+    console.log(person);
     try{
-        const updatedPerson = await Person.updateOne({_id: id}, person)
+        const updatedPerson = await Person.updateOne({_id: id}, person);
+
+        if(updatedPerson.matchedCount === 0){
+            res.status(422).json({
+                message: 'Usuario nao encontrado'
+            })
+            return 
+        }
+
         res.status(200).json(person);
     }catch(error){
         res.status(500).json({error: error});
 
     }
+
+})
+
+// Delete
+
+router.delete('/:id', async(req, res)=>{
+    const id = req.params.id
+
+    const person = await Person.findOne({ _id: id})
+
+     if(!person){
+        res.status(422).json({
+            message: 'Usuario nao encontrado'
+        })
+        return
+    }
+
+    try{
+        await Person.deleteOne({_id: id})
+        res.status(200).json({message: 'Usuário removido com sucesso.'})
+    }catch(error){
+        res.status(500).json({error: error});
+
+    }
+
 
 })
 
