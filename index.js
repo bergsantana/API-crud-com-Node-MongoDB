56eee9212e69bc69e11bc1c1d95fd2b4f81c1b2e
Wanderberg Santana <99621171+bergsantana@users.noreply.github.com>
2024-04-17 13:12:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -32,12 +32,12 @@ app.get('/',( req, res) => {
 })
 
 // Entregar uma porta
-const DB_USER = 'berg'
+const DB_USER = ' '
 const DB_PASSWORD = ' '
 
 mongoose
     .connect(
-    `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.afvkx3s.mongodb.net/bancodaapi?retryWrites=true&w=majority`
+    ` `
 )
     .then(()=>{
         console.log('Conectamos ao MongoDB');
